01cbbdf6184cdb309bbafa1ee20c90dd899072f9
Stela Oliveira <stelaoliver2020@gmail.com>
2021-06-13 22:13:39 -0300

    Formato fot insta

---
+++ me_segue_no_insta.js
@@ -0,0 +1,18 @@
+function solucao(largura, altura) 
+{
+    if(altura > largura)
+    {
+        console.log("RETRATO");
+    }
+    else
+    {
+        if(altura < largura)
+        {
+          console.log("PAISAGEM");   
+        }
+        else
+        {
+            console.log("QUADRADA");
+        }
+    }
+}
